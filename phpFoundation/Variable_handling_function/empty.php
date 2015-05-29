3baf7648a67eb46aa8e50c589e2ba87bc877314d
Chanpa_Majumder <chanpacste@gmail.com>
2015-05-29 11:26:16 +0600

    edited empty.php

--- phpFoundation/Variable_handling_function/empty.php
+++ phpFoundation/Variable_handling_function/empty.php
@@ -1,12 +1,12 @@
 <?php
-$var = 0;
+$val = 1;
 
 
-if (empty($var)) {
-    echo '$var is either 0, empty, or not set at all';
+if (empty($val)) {
+    echo '$val is either 0, empty, or not set at all';
 }
 
-if (isset($var)) {
-    echo '$var is set even though it is empty';
+if (isset($val)) {
+    echo '$val is set even though it is empty';
 }
 ?>
